f24811ca42abc51a2f057ba38b6ae9d4a11c6410
Timothy J Fontaine <tjfontaine@gmail.com>
2011-07-24 11:48:11 -0400

    load from zone ds, pull handleRequest out of init

---
+++ zones.js
@@ -0,0 +1,38 @@
+var zones = {
+  'fake.atxconsulting.com': {
+    ttl: 300,
+    email: 'tjfontaine.atxconsulting.com',
+    delegates: {
+      'ns1.fake.atxconsulting.com': {
+        ips: [ '127.0.0.1'],
+        ttl: 60,
+      },
+    },
+    resources: {
+      'www': {
+        type: 'a',
+        ttl: 60,
+        handler: require('./lib/policy').LeastLoad(3),
+        nodes: {
+          node1: {
+            ips: [ '127.0.0.1', '127.0.1.1' ],
+            load: 123,
+          },
+          node2: {
+            ips: [ '127.0.0.2' ],
+            load: 128,
+          },
+          node3: {
+            ips: [ '127.0.0.3' ],
+            load: 5,
+          },
+          node4: {
+            ips: [ '127.0.0.4' ],
+            load: 15,
+          },
+        },
+      },
+    },
+  }
+}
+exports.zones = zones
