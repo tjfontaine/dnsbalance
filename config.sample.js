affbc542995b166750d40967284399b34a052eb9
Timothy J Fontaine <tjfontaine@gmail.com>
2012-02-26 11:10:57 -0500

    update config example

--- config.sample.js
+++ config.sample.js
@@ -5,17 +5,6 @@
       "port": 5353
     }
   ],
-  "rpc": [
-    {
-      "ip": "0.0.0.0",
-      "port": 5454
-    }
-  ],
-  "delegates": [
-    {
-      "ip": "ns1.example.com",
-      "port": 5454
-    }
-  ],
+  "database": "mongodb://localhost/dnsbalance",
   "zones_directory": "./zones"
 }
