5448709bd4131e9501a1758ad4844d5b07a467e2
Timothy J Fontaine <tjfontaine@gmail.com>
2012-12-08 22:29:53 -0500

    simplify serverrequest, remove chainsaw dep

--- config.sample.js
+++ config.sample.js
@@ -1,4 +1,14 @@
 {
+  acl: {
+    local: {
+      recursion: true,
+      ip: '127.0.0.1',
+    },
+  },
+  forwarders: [
+    '8.8.8.8',
+    '8.8.4.4',
+  ],
   query: [{
     ip: '0.0.0.0',
     port: 15353,
